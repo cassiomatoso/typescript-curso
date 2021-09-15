d4b0aa85e116fae1802e718a044058c2be8288a2
cassiomatoso <cassiocg98@gmail.com>
2021-09-15 17:03:17 -0300

    Mais sobre readonly

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,12 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    constructor(
+    //publico mas somente para leitura
+    data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
