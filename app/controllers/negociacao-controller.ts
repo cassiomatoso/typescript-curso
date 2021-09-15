ca89a0a72792792f603cbb50f192144a494a997e
cassiomatoso <cassiocg98@gmail.com>
2021-09-15 14:26:10 -0300

    Adicionando tipagem

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,17 +1,26 @@
+import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
-    private inputData
-    private inputQuantidade
-    private inputValor
 
-    constructor() {
-        this.inputData = document.querySelector('#data')
-        this.inputQuantidade = document.querySelector('#quantidade')
-        this.inputValor = document.querySelector('#valor')
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor(data: Date, quantidade: number, valor: number) {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona(){
-        console.log(this.inputData)
-        console.log(this.inputQuantidade)
-        console.log(this.inputValor)
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
     }
 }
