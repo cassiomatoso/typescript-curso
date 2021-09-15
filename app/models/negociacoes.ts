bb6e90fcc87a2ed0b0f957ba391e3a447dd34ee5
cassiomatoso <cassiocg98@gmail.com>
2021-09-15 16:58:21 -0300

    Arrays e Generics - outras formas de uso

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    //private negociacoes: Array<Negociacao> - é a mesma coisa 
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
     //lista somente para leitura
-    lista(): ReadonlyArray<Negociacao> {
+    //lista(): ReadonlyArray<Negociacao> - é a mesma coisa
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,11 +1,13 @@
 export class Negociacoes {
     constructor() {
+        //private negociacoes: Array<Negociacao> - é a mesma coisa 
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     //lista somente para leitura
+    //lista(): ReadonlyArray<Negociacao> - é a mesma coisa
     lista() {
         return this.negociacoes;
     }
