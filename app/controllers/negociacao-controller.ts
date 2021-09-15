ac9f27ebda67a45e55552d15c96ff5d8df852176
cassiomatoso <cassiocg98@gmail.com>
2021-09-15 14:35:24 -0300

    Limpando o formulário

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -5,7 +5,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
 
-    constructor(data: Date, quantidade: number, valor: number) {
+    constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
@@ -14,6 +14,7 @@ export class NegociacaoController {
     adiciona(): void {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
@@ -22,5 +23,12 @@ export class NegociacaoController {
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
