01034757fca801ad1c3c391afe22374b831c0357
cassiomatoso <cassiocg98@gmail.com>
2021-09-16 18:42:42 -0300

    Criando table + data formatada

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,3 +1,5 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociacoesView {
 
     private elemento: HTMLElement;
@@ -6,26 +8,37 @@ export class NegociacoesView {
         this.elemento = document.querySelector(seletor);
     }
 
-    //declara o template da view
-    template(): string {
+    //método que declara o template da view
+    template(model: Negociacoes): string {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
                     <tr>
-                    <th> DATA </th>
-                    <th> QUANTIDADE </th>
-                    <th> VALOR </th>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
                     </tr>
                 </thead>
                 <tbody>
+                    ${model.lista().map(negociacao => {
+                        return `
+                            <tr>
+                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            <tr>
+                        `;
+                    }).join('')}
                 </tbody>
             </table>
         `;
     }
 
-    //renderiza o template no elemento capturado pelo construtor
-    update(): void {
-        this.elemento.innerHTML = this.template();
+    //método que renderiza o template no elemento capturado pelo construtor
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -2,24 +2,35 @@ export class NegociacoesView {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-    //declara o template da view
-    template() {
+    //método que declara o template da view
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
                     <tr>
-                    <th> DATA </th>
-                    <th> QUANTIDADE </th>
-                    <th> VALOR </th>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
                     </tr>
                 </thead>
                 <tbody>
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr>
+                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            <tr>
+                        `;
+        }).join('')}
                 </tbody>
             </table>
         `;
     }
-    //renderiza o template no elemento capturado pelo construtor
-    update() {
-        this.elemento.innerHTML = this.template();
+    //método que renderiza o template no elemento capturado pelo construtor
+    update(model) {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 }
