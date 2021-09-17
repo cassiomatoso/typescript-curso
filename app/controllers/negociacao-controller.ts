575597c9a21be4345132061f72f61ad308b8a42d
cassiomatoso <cassiocg98@gmail.com>
2021-09-17 18:02:19 -0300

    Explicitando visibilidade dos métodos para melhor leitura/entendimento

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -18,14 +18,14 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void {
+    public adiciona(): void {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
     }
 
-    criaNegociacao(): Negociacao {
+    private criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -33,7 +33,7 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
 
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,15 +1,17 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
+    
     //private negociacoes: Array<Negociacao> - é a mesma coisa 
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
+   
     //lista somente para leitura
     //lista(): ReadonlyArray<Negociacao> - é a mesma coisa
-    lista(): readonly Negociacao[] {
+    public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 
